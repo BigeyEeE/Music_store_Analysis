d4bfa6de2be381ab181c33cfc0d0a2ba40fb500d
Tushar SIngh <149139652+BigeyEeE@users.noreply.github.com>
2024-05-15 14:02:09 +0530

    music_Store.sql

--- music_Store.sql
+++ music_Store.sql
@@ -264,3 +264,60 @@ FROM
 WHERE
     MILLISECONDS > AVG_SONG_LENGTH
 	ORDER BY MILLISECONDS DESC;
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+-- Question Set 3 – Advance 
+---------------------------------------------------------------------------------------------------------------------------------------------
+-- 1. Find how much amount spent by each customer on artists? Write a query to return 
+-- customer name, artist name and total spent.
+
+select * from customer
+select * from artist
+select * from invoice_line
+
+	-- solution by join 
+	
+select 	concat(c.first_name,' ',c.last_name) as customer_name,
+		a.name as artist,
+		cast(sum(il.unit_price * il.quantity) as numeric(10,2)) as total_spent
+	from customer c
+	join 
+	invoice as i on
+	c.customer_id = i.customer_id
+	join
+	invoice_line as il on
+	i.invoice_id = il.invoice_id 
+	join 
+	track as t on 
+	il.track_id = t.track_id
+	join 
+	album as ab on 
+	t.album_id = ab.album_id
+	join artist as a on 
+	ab.artist_id = a.artist_id	
+	where a.name = 'Queen'      -- most spend on artists
+	group by 1,2
+    ORDER BY 
+   total_spent desc;
+
+-- solution through cte 
+
+WITH best_selling_artist AS (
+	SELECT artist.artist_id AS artist_id, artist.name AS artist_name, SUM(invoice_line.unit_price*invoice_line.quantity) AS total_sales
+	FROM invoice_line
+	JOIN track ON track.track_id = invoice_line.track_id
+	JOIN album ON album.album_id = track.album_id
+	JOIN artist ON artist.artist_id = album.artist_id
+	GROUP BY 1,2
+	ORDER BY 3 DESC
+	LIMIT 1
+)
+SELECT c.customer_id, c.first_name, c.last_name, bsa.artist_name, SUM(il.unit_price*il.quantity) AS amount_spent
+FROM invoice i
+JOIN customer c ON c.customer_id = i.customer_id
+JOIN invoice_line il ON il.invoice_id = i.invoice_id
+JOIN track t ON t.track_id = il.track_id
+JOIN album alb ON alb.album_id = t.album_id
+JOIN best_selling_artist bsa ON bsa.artist_id = alb.artist_id
+GROUP BY 1,2,3,4
+ORDER BY 5 DESC;
